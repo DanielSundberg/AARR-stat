848d96454ebfd64b0ba6f1a7cd58b8f610cd3454
Daniel Sundberg <sumpan@gmail.com>
2020-09-17 12:38:23 +0200

    Add AARR stat controller with sample data

---
+++ AARRStatSessionItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AARR_stat
+{
+    public class AARRStatSessionItem
+    {
+        public string User { get; set; }
+        public string Device { get; set; }
+        public string Session { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
---
+++ Controllers/AARRStatController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AARR_stat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AARRStatController : ControllerBase
+    {
+        private readonly ILogger<AARRStatController> _logger;
+
+        public AARRStatController(ILogger<AARRStatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<AARRStatSessionItem> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new AARRStatSessionItem
+            {
+                User = Guid.NewGuid().ToString(),
+                Device = Guid.NewGuid().ToString(),
+                Session = Guid.NewGuid().ToString(),
+                Start = DateTime.Now.AddDays(index).ToUniversalTime(),
+                End = DateTime.Now.AddDays(index).AddMinutes(5).ToUniversalTime(),
+            })
+            .ToArray();
+        }
+    }
+}
